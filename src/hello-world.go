f079f1e2195a3d70f70ea4654e977ae65b0fba02
kiengithub <94677260+kiengithub@users.noreply.github.com>
2024-12-01 16:12:39 +0700

    Update hello-world.go

--- src/hello-world.go
+++ src/hello-world.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-    message := "MESSAGE - cicd - 05"
+    message := "MESSAGE - cicd - 06"
 
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, message)

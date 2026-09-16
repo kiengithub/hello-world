0b5355d10389fd4ada9d2afa527559adab004d8b
kiengithub <94677260+kiengithub@users.noreply.github.com>
2026-09-16 23:17:00 +0700

    sss

--- src/hello-world.go
+++ src/hello-world.go
@@ -1,17 +1,28 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
+	"time"
 )
 
 func main() {
-    message := "MESSAGE - cicd-main - 02 -02"
+	message := "MESSAGE - cicd-main - 02"
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, message)
-    })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, message)
+	})
 
-    fmt.Println("Starting server on port 8080.")
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/slow", func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		fmt.Println("slow: bat dau xu ly luc", start.Format(time.RFC3339))
+		time.Sleep(3 * time.Minute)
+		end := time.Now()
+		fmt.Println("slow: xu ly xong luc", end.Format(time.RFC3339))
+		fmt.Fprintf(w, "OK - da xu ly xong sau %v (bat dau: %s, ket thuc: %s)\n",
+			end.Sub(start), start.Format(time.RFC3339), end.Format(time.RFC3339))
+	})
+
+	fmt.Println("Starting server on port 8080.")
+	http.ListenAndServe(":8080", nil)
 }
